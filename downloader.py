e451b76b120fcac1b4729f71fcdb506bbaad506f
İsa Türk <isaturk07@gmail.com>
2023-12-02 05:43:21 +0300

    Refactor URLDownloader class and add command line arguments

--- downloader.py
+++ downloader.py
@@ -1,16 +1,10 @@
-import requests
 from concurrent.futures import ThreadPoolExecutor
 import os
 import time
+import argparse
+
 
 from utility.request_tool import RequestTool
-
-# Constants
-URL_LIST_FILE = "info_urls.txt"
-DOWNLOAD_DIR = "dergipark_htmls"
-MAX_WORKERS = 20
-MAX_RETRIES = 3
-RETRY_BACKOFF = 0.5  # seconds
 
 
 import unicodedata
@@ -39,10 +33,16 @@ def slugify(value, allow_unicode=False):
 
 
 class URLDownloader:
-    def __init__(self, url_list, download_dir):
+    def __init__(
+        self, url_list, download_dir, max_workers=20, max_retries=3, retry_backoff=0.5
+    ):
         self.url_list = url_list
         self.download_dir = download_dir
         self.downloaded_count = 0
+        self.max_workers = max_workers
+        self.max_retries = max_retries
+        self.retry_backoff = retry_backoff
+
         self.total_urls = len(url_list)
         if not os.path.exists(download_dir):
             os.makedirs(download_dir)
@@ -64,14 +64,14 @@ class URLDownloader:
             else:
                 raise Exception(f"HTTP Error: {response.status_code}")
         except Exception as e:
-            if retry_count < MAX_RETRIES:
+            if retry_count < self.max_retries:
                 print(
-                    f"Error downloading {url}: {e}. Retrying... ({retry_count+1}/{MAX_RETRIES})"
+                    f"Error downloading {url}: {e}. Retrying... ({retry_count+1}/{self.max_retries})"
                 )
-                time.sleep(RETRY_BACKOFF * retry_count)
+                time.sleep(self.retry_backoff * retry_count)
                 self.download_url(url, retry_count + 1)
             else:
-                print(f"Failed to download {url} after {MAX_RETRIES} retries.")
+                print(f"Failed to download {url} after {self.max_retries} retries.")
 
     def _update_progress(self):
         self.downloaded_count += 1
@@ -79,7 +79,7 @@ class URLDownloader:
         print(f"Progress: {progress:.2f}%")
 
     def start_download(self):
-        with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
             try:
                 executor.map(self.download_url, self.url_list)
             except KeyboardInterrupt:
@@ -92,16 +92,72 @@ def read_url_list(file_path):
         return [line.strip() for line in file]
 
 
-def main():
-    REQUEST_FILE = "proxy_file.json"
+def main(args):
+    # Constants
+    PROXY_FILE = args.proxy
+    URL_LIST_FILE = args.url_list
+    DOWNLOAD_DIR = args.download_dir
+    MAX_WORKERS = args.max_workers
+    MAX_RETRIES = args.max_retries
+    RETRY_BACKOFF = args.retry_backoff
 
     request_tool = RequestTool()
-    request_tool.read_from_proxy_file(REQUEST_FILE)
+    request_tool.read_from_proxy_file(PROXY_FILE)
 
     url_list = read_url_list(URL_LIST_FILE)
-    downloader = URLDownloader(url_list, DOWNLOAD_DIR)
+    downloader = URLDownloader(
+        url_list, DOWNLOAD_DIR, MAX_WORKERS, MAX_RETRIES, RETRY_BACKOFF
+    )
     downloader.start_download()
 
 
+def get_args():
+    parser = argparse.ArgumentParser(description="Download HTML files from DergiPark")
+    parser.add_argument(
+        "-p",
+        "--proxy",
+        type=str,
+        help="Proxy file path",
+        default="proxy_list.txt",
+    )
+    parser.add_argument(
+        "-u",
+        "--url-list",
+        type=str,
+        help="URL list file path",
+        required=True,
+    )
+    parser.add_argument(
+        "-d",
+        "--download-dir",
+        type=str,
+        help="Download directory",
+        default="downloads",
+    )
+    parser.add_argument(
+        "-w",
+        "--max-workers",
+        type=int,
+        help="Maximum number of workers",
+        default=20,
+    )
+    parser.add_argument(
+        "-r",
+        "--max-retries",
+        type=int,
+        help="Maximum number of retries",
+        default=3,
+    )
+    parser.add_argument(
+        "-b",
+        "--retry-backoff",
+        type=float,
+        help="Retry backoff time in seconds",
+        default=0.5,
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    main()
+    args = get_args()
+    main(args)
